e599b4f816dc874c34bdde19e4b674cebd090215
Martin Aarhof <maa@scandesigns.dk>
2012-07-27 06:21:57 +0200

    made the design and templates

--- typo3conf/ext/html5_readykit/ext_tables.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/html5_readykit_config/', 'HTML5 Readykit (config)');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/html5_readykit_includes/', 'HTML5 Readykit (includes)');
-?>
--- typo3conf/localconf_original.php
+++ typo3conf/localconf_original.php
@@ -1,5 +1,5 @@
 <?php
-$TYPO3_CONF_VARS['BE']['installToolPassword'] = '';	// Modified or inserted by TYPO3 Install Tool.
+$TYPO3_CONF_VARS['BE']['installToolPassword'] = 'a5d74d2a0d1f855dc174f6fb19f2b1c4';	// Modified or inserted by TYPO3 Install Tool.
 ## INSTALL SCRIPT EDIT POINT TOKEN - all lines after this points may be changed by the install script!
 
 $typo_db_username = '';	//  Modified or inserted by TYPO3 Install Tool.
@@ -23,8 +23,8 @@ $TYPO3_CONF_VARS['BE']['versionNumberInFilename'] = '0';	//  Modified or inserte
 $TYPO3_CONF_VARS['BE']['forceCharset'] = '';	//  Modified or inserted by TYPO3 Install Tool.
 $TYPO3_CONF_VARS['SYS']['compat_version'] = '4.6';	//  Modified or inserted by TYPO3 Install Tool.
 // Updated by TYPO3 Install Tool 26-07-12 17:37:21
-$TYPO3_CONF_VARS['EXT']['extList'] = 'css_styled_content,extbase,filelist,version,tsconfig_help,context_help,extra_page_cm_options,impexp,belog,about,cshmanual,aboutmodules,setup,opendocs,install,t3editor,felogin,feedit,recycler,info,perm,func,simulatestatic,reports,scheduler,t3skin,fluid,workspaces,lowlevel,beuser,func_wizards,wizard_crpages,wizard_sortpages,info_pagetsconfig,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,viewpage,statictemplates,static_info_tables,templavoila,html5_readykit,powermail,pagebrowse,mm_forum,rgsmoothgallery,rsaauth,saltedpasswords,div2007,sr_feuser_register';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager. 
-$TYPO3_CONF_VARS['EXT']['extList_FE'] = 'css_styled_content,extbase,version,install,felogin,feedit,simulatestatic,t3skin,fluid,workspaces,statictemplates,static_info_tables,templavoila,html5_readykit,powermail,pagebrowse,mm_forum,rgsmoothgallery,rsaauth,saltedpasswords,div2007,sr_feuser_register';	// Modified or inserted by TYPO3 Extension Manager. 
+$TYPO3_CONF_VARS['EXT']['extList'] = 'css_styled_content,extbase,filelist,version,tsconfig_help,context_help,extra_page_cm_options,impexp,belog,about,cshmanual,aboutmodules,setup,opendocs,install,t3editor,felogin,feedit,recycler,info,perm,func,simulatestatic,reports,scheduler,t3skin,fluid,workspaces,lowlevel,beuser,func_wizards,wizard_crpages,wizard_sortpages,info_pagetsconfig,tstemplate,tstemplate_ceditor,tstemplate_info,tstemplate_objbrowser,tstemplate_analyzer,viewpage,statictemplates,static_info_tables,templavoila,powermail,pagebrowse,mm_forum,rgsmoothgallery,rsaauth,saltedpasswords,div2007,sr_feuser_register,rtehtmlarea_api_manual,rtehtmlarea';	// Modified or inserted by TYPO3 Extension Manager. Modified or inserted by TYPO3 Core Update Manager. 
+$TYPO3_CONF_VARS['EXT']['extList_FE'] = 'css_styled_content,extbase,version,install,felogin,feedit,simulatestatic,t3skin,fluid,workspaces,statictemplates,static_info_tables,templavoila,powermail,pagebrowse,mm_forum,rgsmoothgallery,rsaauth,saltedpasswords,div2007,sr_feuser_register,rtehtmlarea_api_manual,rtehtmlarea';	// Modified or inserted by TYPO3 Extension Manager. 
 // Updated by TYPO3 Extension Manager 26-07-12 17:37:25
 $TYPO3_CONF_VARS['INSTALL']['wizardDone']['tx_coreupdates_installsysexts'] = '1';	//  Modified or inserted by TYPO3 Upgrade Wizard.
 // Updated by TYPO3 Upgrade Wizard 26-07-12 17:37:25
@@ -38,5 +38,5 @@ $TYPO3_CONF_VARS['EXT']['extConf']['css_styled_content'] = 'a:2:{s:15:"setPageTS
 $TYPO3_CONF_VARS['EXT']['extConf']['templavoila'] = 'a:1:{s:9:"staticDS.";a:3:{s:6:"enable";s:1:"0";s:8:"path_fce";s:27:"fileadmin/templates/ds/fce/";s:9:"path_page";s:28:"fileadmin/templates/ds/page/";}}';	// Modified or inserted by TYPO3 Extension Manager. 
 $TYPO3_CONF_VARS['EXT']['extConf']['powermail'] = 'a:4:{s:12:"disableIpLog";s:1:"0";s:20:"disableBackendModule";s:1:"0";s:24:"disablePluginInformation";s:1:"0";s:13:"enableCaching";s:1:"0";}';	//  Modified or inserted by TYPO3 Extension Manager.
 $TYPO3_CONF_VARS['EXT']['extConf']['sr_feuser_register'] = 'a:3:{s:12:"uploadFolder";s:27:"uploads/tx_srfeuserregister";s:10:"imageTypes";s:30:"png, jpg, jpeg, gif, tif, tiff";s:12:"imageMaxSize";s:3:"500";}';	//  Modified or inserted by TYPO3 Extension Manager.
-// Updated by TYPO3 Extension Manager 26-07-12 17:57:35
+// Updated by TYPO3 Extension Manager 27-07-12 05:40:54
 ?>
