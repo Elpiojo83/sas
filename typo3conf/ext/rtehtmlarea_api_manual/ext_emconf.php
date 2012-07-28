a7d767599a0b87e1dd82424190bd699e9eff9c7c
Martin Aarhof <maa@scandesigns.dk>
2012-07-28 15:53:09 +0200

    added extensions

---
+++ typo3conf/ext/rtehtmlarea_api_manual/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "rtehtmlarea_api_manual".
+#
+# Auto generated 27-07-2012 05:39
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'htmlArea RTE API Manual',
+	'description' => 'Documentation of the htmlArea RTE API',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '2.2.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Stanislas Rolland',
+	'author_email' => 'typo3(arobas)sjbr.ca',
+	'author_company' => 'SJBR',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:2:{s:12:"ext_icon.gif";s:4:"2f41";s:14:"doc/manual.sxw";s:4:"d76c";}',
+);
+
+?>
